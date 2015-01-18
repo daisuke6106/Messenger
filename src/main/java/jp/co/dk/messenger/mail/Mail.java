c9ae91c074d6e575134a6dfce8f8d6cb78ba6611
Daisuke Kanno <daisuke6106@gmail.com>
2015-01-19 02:20:20 +0900

    パッケージ構成を変更 ソースディレクトリからプロパティ、メッセージファイル、テストｙ用ファイル群をresourcesディレクトリに分離
    src→src/main/java、src/main/resources
    test→src/test/java、src/test/resources

---
+++ src/main/java/jp/co/dk/messenger/mail/Mail.java
@@ -0,0 +1,30 @@
+package jp.co.dk.messenger.mail;
+
+import javax.mail.Session;
+
+import jp.co.dk.messenger.Messege;
+import jp.co.dk.messenger.Messenger;
+import jp.co.dk.messenger.exception.MessengerInitializeException;
+import jp.co.dk.messenger.messege.MessengerMessege;
+
+public class Mail implements Messenger{
+	
+	/** SMTPサーバ名 */
+	protected String smtpServerName;
+	
+	/** SMTPホスト名 */
+	protected String smtpHostName;
+	
+	public Mail(String smtpServerName, String smtpHostName) throws MessengerInitializeException {
+		if (smtpServerName == null || smtpServerName.equals("")) throw new MessengerInitializeException(MessengerMessege.ERROR_SERVERNAME_IS_NOT_SET);
+		if (smtpHostName   == null || smtpHostName.equals(""))   throw new MessengerInitializeException(MessengerMessege.ERROR_HOSTNAME_IS_NOT_SET);
+		this.smtpServerName = smtpServerName;
+		this.smtpHostName   = smtpHostName;
+	}
+
+	@Override
+	public void send(Messege message) {
+		
+	}
+
+}
